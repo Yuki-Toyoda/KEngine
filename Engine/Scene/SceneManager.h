279b556e4701a92a578e418621cc0894b6115fd2
ToyodaYuki <115054848+Yuki-Toyoda@users.noreply.github.com>
2023-10-17 12:26:16 +0900

    シーンの管理方法を変更 次のシーンをシーンマネージャが持つように

--- Engine/Scene/SceneManager.h
+++ Engine/Scene/SceneManager.h
@@ -32,10 +32,20 @@ public: // メンバ関数
 	/// </summary>
 	void Update();
 
+public: // アクセッサ等
+
+	/// <summary>
+	/// 次のシーンのセッター
+	/// </summary>
+	/// <param name="nextScene">次のシーンの実体</param>
+	void SetNextScene(BaseScene* nextScene) { nextScene_ = nextScene; }
+
 private: // メンバ変数
 	
 	// 現在のシーン
-	BaseScene* currentScene_;
+	BaseScene* currentScene_ = nullptr;
+	// 次のシーン
+	BaseScene* nextScene_ = nullptr;
 
 };
 
