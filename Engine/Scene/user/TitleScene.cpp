3c2291220623419d207c94a73d1b11bc3e3c3dd3
ToyodaYuki <115054848+Yuki-Toyoda@users.noreply.github.com>
2023-10-18 11:39:35 +0900

    おい！

--- Engine/Scene/user/TitleScene.cpp
+++ Engine/Scene/user/TitleScene.cpp
@@ -7,9 +7,11 @@ void TitleScene::Initialize()
 
 void TitleScene::Update()
 {
+
 	// 右シフトが押されたら次のシーンへ
 	if (input_->TriggerKey(DIK_RSHIFT)) {
 		BaseScene* nextScene = new GameScene();
 		SceneManager::GetInstance()->SetNextScene(nextScene);
 	}
+
 }
