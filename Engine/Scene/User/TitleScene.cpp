48bec89463cdb8c88a4579b09cc1f19d8eee43b9
ToyodaYuki <115054848+Yuki-Toyoda@users.noreply.github.com>
2023-11-22 15:11:49 +0900

    不要な関数を削除

--- Engine/Scene/User/TitleScene.cpp
+++ Engine/Scene/User/TitleScene.cpp
@@ -9,21 +9,6 @@ void TitleScene::Initialize()
 
 void TitleScene::Update()
 {
-
-	Quaternion rotation = Math::MakeRotateAxisAngleQuaternion(
-		Math::Normalize(Vector3{ 1.0f, 0.4f, -0.2f }), 0.45f);
-	Vector3 pointY = { 2.1f, -0.9f, 1.3f };
-	Matrix4x4 rotateMatrix = Math::QuaternionToMatrix(rotation);
-	Vector3 rotateByQuaternion = Math::RoatateVector(pointY, rotation);
-	Vector3 rotateByMatrix = Math::Transform(pointY, rotateMatrix);
-
-	ImGui::Begin("answer");
-	ImGui::DragFloat4("rotation", &rotation.x, 0.0f);
-	Math::MatrixImGui(rotateMatrix, "rotateMatrix");
-	ImGui::DragFloat3("rotateByQuaternion", &rotateByQuaternion.x, 0.0f);
-	ImGui::DragFloat3("rotateByMatrix", &rotateByMatrix.x, 0.0f);
-	ImGui::End();
-
 	// デバッグ時のみ特定のキーでシーン遷移
 #ifdef _DEBUG
 	// デバッグ遷移
