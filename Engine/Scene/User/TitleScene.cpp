3121d516b5ce322263205e40ed2384dd4cc9f71a
ToyodaYuki <115054848+Yuki-Toyoda@users.noreply.github.com>
2023-12-06 14:12:07 +0900

    MT4_01_03_確認課題修了

--- Engine/Scene/User/TitleScene.cpp
+++ Engine/Scene/User/TitleScene.cpp
@@ -4,20 +4,30 @@
 
 void TitleScene::Initialize()
 {
-	SampleBox* box = nullptr;
-	box = GameObjectManager::GetInstance()->CreateInstance<SampleBox>("box", BaseObject::TagNone);
-	box->transform_.translate_ = { 3.0f, 0.0f, 0.0f };
-
-	box = GameObjectManager::GetInstance()->CreateInstance<SampleBox>("box", BaseObject::TagPlayer);
+	
 }
 
 void TitleScene::Update()
 {
-	ImGui::Begin("Generate");
-	if (ImGui::Button("Generate Plane")) {
-		SampleBox* box = nullptr;
-		box = GameObjectManager::GetInstance()->CreateInstance<SampleBox>("box", BaseObject::TagPlayer);
-	}
+	
+	Quaternion q1 = { 2.0f, 3.0f, 4.0f, 1.0f };
+	Quaternion q2 = { 1.0f, 3.0f, 5.0f, 2.0f };
+	Quaternion identity = Math::MakeIdentityQuaternion();
+	Quaternion conj = Math::Conjugate(q1);
+	Quaternion inv = Math::Inverse(q1);
+	Quaternion normal = Math::Normalize(q1);
+	Quaternion mul1 = q1 * q2;
+	Quaternion mul2 = q2 * q1;
+	float norm = Math::Length(q1);
+
+	ImGui::Begin("Answer");
+	ImGui::DragFloat4("identity", &identity.x, 0.05f);
+	ImGui::DragFloat4("conj", &conj.x, 0.05f);
+	ImGui::DragFloat4("inv", &inv.x, 0.05f);
+	ImGui::DragFloat4("normal", &normal.x, 0.05f);
+	ImGui::DragFloat4("mul1", &mul1.x, 0.05f);
+	ImGui::DragFloat4("mul2", &mul2.x, 0.05f);
+	ImGui::DragFloat("norm", &norm, 0.05f);
 	ImGui::End();
 
 	// デバッグ時のみ特定のキーでシーン遷移
