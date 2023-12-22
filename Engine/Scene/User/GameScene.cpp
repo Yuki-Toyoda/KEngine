76d57094ed022e4fc5fdb078e4b348b592784124
k022g0019@g.neec.ac.jp <k022g0019@g.neec.ac.jp>
2023-12-22 14:09:00 +0900

    くっつくところまでできたけどFPSが３０しか出ない

--- Engine/Scene/User/GameScene.cpp
+++ Engine/Scene/User/GameScene.cpp
@@ -8,19 +8,19 @@ void GameScene::Init(){
 	SkyDome* skyDome = nullptr;
 	skyDome = gameObjectManager_->CreateInstance<SkyDome>("SkyDome", BaseObject::TagNone);
 
-	/*Player* player = nullptr;
+	Player* player = nullptr;
 	player = gameObjectManager_->CreateInstance<Player>("Player", BaseObject::TagPlayer);
 
 	Wepon* wepon = nullptr;
 	wepon = gameObjectManager_->CreateInstance<Wepon>("wepon", BaseObject::TagPlayer);
 	wepon->SetTarget(&player->transform_);
-	wepon->transform_.SetParent(&player->transform_, 0b001);*/
-	/*Enemy* enemy ;
+	wepon->transform_.SetParent(&player->transform_, 0b001);
+	Enemy* enemy ;
 	for (int i = 0; i < 15; i++) {
 		enemy = gameObjectManager_->CreateInstance<Enemy>("Enemy", BaseObject::TagEnemy);
 		enemy->transform_.translate_.x = i * 2.0f;
 		enemy->SetWepon(wepon);
-	}*/
+	}
 
 
 }
