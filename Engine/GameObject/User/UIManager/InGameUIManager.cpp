7c7684addc85b8bfc4a222bcf237c7bf5350ef1d
ToyodaYuki <115054848+Yuki-Toyoda@users.noreply.github.com>
2024-01-21 13:50:26 +0900

    IngameSceneのUIを実装

--- Engine/GameObject/User/UIManager/InGameUIManager.cpp
+++ Engine/GameObject/User/UIManager/InGameUIManager.cpp
@@ -5,13 +5,13 @@ void InGameUIManager::Init()
 {
 	// スプライトの追加
 	AddSprite("PauseMenu", { 0.0f, 0.0f }, { 224.0f, 64.0f }, TextureManager::Load("./Resources/UI/InGame", "pauseBotton.png"));
-	AddSprite("Slam", { 880.0f, 80.0f }, { 384.0f, 64.0f }, TextureManager::Load("./Resources/UI/InGame", "slam.png"));
-	AddSprite("MoveUI_BG", { 1020.0f, 0.0f }, { 232.0f, 85.3f }, TextureManager::Load("./Resources/UI/InGame", "moveBack.png"));
-	AddSprite("MoveUI_F", { 1022.0f, 0.0f }, { 232.0f, 85.3f }, TextureManager::Load("./Resources/UI/InGame", "moveFront.png"));
+	AddSprite("Attack", { 980.0f, 95.0f }, { 298.0f, 64.0f }, TextureManager::Load("./Resources/UI/InGame", "attack.png"));
+	AddSprite("MoveUI_BG", { 990.0f, 0.0f }, { 288.0f, 96.0f }, TextureManager::Load("./Resources/UI/InGame", "moveBack.png"));
+	AddSprite("MoveUI_F", { 990.0f, 0.0f }, { 288.0f, 96.0f }, TextureManager::Load("./Resources/UI/InGame", "moveFront.png"));
 
 	// スプライトの描画範囲設定
 	sprites_[0]->texSize_ = { 448.0f, 128.0f };
-	sprites_[1]->texSize_ = { 768.0f, 128.0f };
+	sprites_[1]->texSize_ = { 448.0f, 96.0f };
 
 	// 入力取得
 	input_ = Input::GetInstance();
@@ -35,7 +35,7 @@ void InGameUIManager::Update()
 	move = Math::Normalize(move) * 10.0f;
 
 	// スティックの結果をUIの反映
-	sprites_[3]->translate_ = Vector2(1022.0f, 0.0f) + Vector2(move.x, move.z);
+	sprites_[3]->translate_ = Vector2(990.0f, 0.0f) + Vector2(move.x, move.z);
 
 	// Aボタンを押すとUIを変化させる
 	if (joyState_.Gamepad.wButtons & XINPUT_GAMEPAD_START) {
@@ -50,7 +50,7 @@ void InGameUIManager::Update()
 	// Aボタンを押すとUIを変化させる
 	if (joyState_.Gamepad.wButtons & XINPUT_GAMEPAD_A) {
 		// UIを変更する
-		sprites_[1]->texBase_ = Vector2(768.0f, 0.0f);
+		sprites_[1]->texBase_ = Vector2(448.0f, 0.0f);
 	}
 	else {
 		// UIを変更する
