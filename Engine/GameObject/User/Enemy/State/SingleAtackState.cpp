7ec8230fc5f48e982e296a4a43d0f238b36ed8e5
Hnaoyan <k022g0041@g.neec.ac.jp>
2024-01-22 15:35:52 +0900

    出現の高さ変更

--- Engine/GameObject/User/Enemy/State/SingleAtackState.cpp
+++ Engine/GameObject/User/Enemy/State/SingleAtackState.cpp
@@ -7,11 +7,6 @@ void SingleAtackState::Init()
 
 void SingleAtackState::Update()
 {
-	////隕石を生成してstateを変更
-	//Meteor* meteor=gameObjectmanager_->CreateInstance<Meteor>("meteor", BaseObject::TagMeteor);
-	////隕石の座標
-	//meteor->transform_.translate_.y = 30.0f;
-	//meteor->transform_.translate_.z = -10.0f;
 	//隕石を生成してstateを変更
 	GameDataManager* jsonData = GameDataManager::GetInstance();
 	// 参照の名前
@@ -23,7 +18,7 @@ void SingleAtackState::Update()
 		//隕石の座標
 		std::string paramName = "Meteor" + std::to_string(i);
 		meteor->transform_.translate_ = jsonData->GetVector3Value({ "SingleMeteor",paramName }, "Position");
-		meteor->transform_.translate_.y = 30.0f;
+		meteor->transform_.translate_.y = jsonData->GetFloatValue({"MeteorParam","Info"},"Distance");
 		meteor->transform_.scale_ = jsonData->GetVector3Value(names, "Scale");
 	}
 
