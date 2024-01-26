e53f497b0905188a5556419274b4032d9ff4690d
k022g0019@g.neec.ac.jp <k022g0019@g.neec.ac.jp>
2024-01-26 22:08:58 +0900

    PushUpの当たり判定を修正

--- Engine/GameObject/User/Enemy/Boss.cpp
+++ Engine/GameObject/User/Enemy/Boss.cpp
@@ -31,19 +31,19 @@ void Boss::SuccessorInit()
 	//下からの攻撃を生成
 	int pushUpMax = dataManager->GetValue<int>({ "AttackParam","PushUp" }, "MaxCount");
 	pushUpMax;
-	// 全ての攻撃に対して
-	//for (int i = 0; i < pushUpMax; i++) {
-	//	// インスタンス生成
-	//	PushUp* pushUp;
-	//	pushUp = gameObjectManager_->CreateInstance<PushUp>("PushUp", BaseObject::TagNone);
-	//	// 名前
-	//	std::string name = "PushUp" + std::to_string(i);
-	//	// Y座標以外を設定
-	//	Vector3 newPos = dataManager->GetValue<Vector3>({ "PushUpAttack",name }, "Position");
-	//	pushUp->transform_.translate_.x = newPos.x;
-	//	pushUp->transform_.translate_.z = newPos.z;
-	//	pushUp_.push_back(pushUp);
-	//}
+	 //全ての攻撃に対して
+	for (int i = 0; i < pushUpMax; i++) {
+		// インスタンス生成
+		PushUp* pushUp;
+		pushUp = gameObjectManager_->CreateInstance<PushUp>("PushUp", BaseObject::TagPushUp);
+		// 名前
+		std::string name = "PushUp" + std::to_string(i);
+		// Y座標以外を設定
+		Vector3 newPos = dataManager->GetValue<Vector3>({ "PushUpAttack",name }, "Position");
+		pushUp->transform_.translate_.x = newPos.x;
+		pushUp->transform_.translate_.z = newPos.z;
+		pushUp_.push_back(pushUp);
+	}
 	GlobalVariables* variables = GlobalVariables::GetInstance();
 	variables->CreateGroup(name_);
 	variables->AddItem(name_, "HitPoint", hitPoint_);
@@ -129,7 +129,11 @@ void Boss::OnCollisionEnter(Collider* collider)
 
 void Boss::MakeStateList()
 {
-	for (int i = 0; i < 1; i++) {
+	stateList_.state_.resize(1);
+	stateList_.state_.at(0).push_back(std::make_unique<WaitTimeState>());
+	stateList_.stateNumber_.resize( 1);
+	stateList_.stateNumber_.at(0).push_back(0);
+	for (int i = 1; i < 2; i++) {
 		//行動状態のリストを作成
 		stateList_.state_.resize(i+1);
 		stateList_.state_.at(i).push_back(MakeState("SingleAtack"));
