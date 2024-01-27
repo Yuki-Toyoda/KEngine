1cff03dce9634e2a929cd6273c9a7156c4b73b5c
ToyodaYuki <115054848+Yuki-Toyoda@users.noreply.github.com>
2024-01-28 03:33:53 +0900

    ゲームマネージャークラス内の変数名と値の書き出し方を変更

--- Engine/GameObject/User/GameManager/GameManager.h
+++ Engine/GameObject/User/GameManager/GameManager.h
@@ -17,12 +17,12 @@ public:
 	float meteorSpeed_=0.2f;
 	//かけらの当たり判定
 	Vector3 rubbleCollisionScale_;
-	//下からの開店時間
-	float pushUpRotateSpeed_=3.0f;
+	//下からの攻撃準備時間
+	float pushUpPreAttackTime_= 1.0f;
 	//下からの攻撃時間
-	float pushUpAtackTime_=3.0f;
-	//下からの攻撃の判定の残る時間
-	float pushUpRemainTime_=3.0f;
+	float pushUpAtackTime_= 0.5f;
+	//下からの攻撃持続時間
+	float pushUpPostAttackTime_= 4.0f;
 	//ローラーの速さ
 	float RollerSpeed_=0.2f;
 	//	かけらの飛ぶ距離の最大
