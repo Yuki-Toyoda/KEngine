8377f9f2f6a2a2d4faffcc795a8a667ad47cd6f9
ToyodaYuki <115054848+Yuki-Toyoda@users.noreply.github.com>
2024-07-05 22:08:02 +0900

    ガウシアンフィルター実装

--- Engine/Resource/Shader/PostProcess/PostProcess.PS.hlsl
+++ Engine/Resource/Shader/PostProcess/PostProcess.PS.hlsl
@@ -18,10 +18,21 @@ Texture2D<float32_t4> gDepth : register(t1);
 SamplerState gSampler : register(s0);
 
 
+struct GaussianData {
+	float32_t intensity;
+};
+
+ConstantBuffer<GaussianData> gaData : register(b1);
+    
+float Gaussian(float x, float sigma)
+{
+    return exp(-0.5 * (x * x) / (sigma * sigma)) / (sqrt(2.0 * 3.1415926535897932384626433832795) * sigma);
+}
+
 struct VignetteData {
 	float32_t intensity;
 };
-ConstantBuffer<VignetteData> vData : register(b1);
+ConstantBuffer<VignetteData> vData : register(b2);
 
 float32_t Vignette(float32_t2 texcoord) {
 	float32_t2 center = float2(0.5f, 0.5f);
@@ -33,7 +44,7 @@ struct GrayScaleData {
 	float32_t intensity;
 };
 
-ConstantBuffer<GrayScaleData> gsData : register(b2);
+ConstantBuffer<GrayScaleData> gsData : register(b3);
     
 float32_t3 GrayScale(float32_t3 color)
 {
@@ -49,6 +60,40 @@ float32_t4 main(PSInput input) : SV_TARGET {
 
 	output = gTexture.Sample(gSampler, uv);
 
+
+    uint32_t width, height;
+    gTexture.GetDimensions(width, height);
+    float32_t2 uvStepSize = float32_t2(rcp(width), rcp(height));
+
+    if (gaData.intensity >= 1.0f)
+    {
+         int kernelRadius = int(ceil(2.0 * gaData.intensity));
+        int kernelSize = kernelRadius * 2 + 1;
+
+        float kernel[49];
+        float totalWeight = 0.0;
+        for (int i = -kernelRadius; i <= kernelRadius; ++i)
+        {
+            kernel[i + kernelRadius] = Gaussian(float(i), gaData.intensity);
+            totalWeight += kernel[i + kernelRadius];
+        }
+
+        float4 color = float4(0, 0, 0, 0);
+
+        for (int y = -kernelRadius; y <= kernelRadius; ++y)
+        {
+            for (int x = -kernelRadius; x <= kernelRadius; ++x)
+            {
+                float2 offset = float2(float(x), float(y)) * uvStepSize;
+                float weight = kernel[x + kernelRadius] * kernel[y + kernelRadius];
+                color += gTexture.Sample(gSampler, input.texcoord + offset) * weight;
+        }
+
+        color /= totalWeight;
+    
+        output = color;
+    }
+    }
 
 	float v = Vignette(input.texcoord);
 	output = float32_t4(output.rgb * v, output.a);
