ddb2751731ff795d611eb94caf047d1371322264
ToyodaYuki <115054848+Yuki-Toyoda@users.noreply.github.com>
2024-07-18 12:30:38 +0900

    死亡時の処理でガウシアンフィルターを掛けないように

--- Engine/GameObject/User/Player/State/Damage.cpp
+++ Engine/GameObject/User/Player/State/Damage.cpp
@@ -44,7 +44,7 @@ void Damage::Update()
 			
 			// カメラのポストプロセスの強さをだんだん上げてく
 			c->ppProcessor_.grayScale_.intensity_ = KLib::Lerp<float>(0.0f, 1.0f, KLib::EaseInQuad(player_->skiningModels_[0]->animationManager_.GetPlayingAnimationProgress()));
-			c->ppProcessor_.gaussian_.intensity_ = KLib::Lerp<float>(0.0f, 3.0f, KLib::EaseInQuad(player_->skiningModels_[0]->animationManager_.GetPlayingAnimationProgress()));
+			//c->ppProcessor_.gaussian_.intensity_ = KLib::Lerp<float>(0.0f, 3.0f, KLib::EaseInQuad(player_->skiningModels_[0]->animationManager_.GetPlayingAnimationProgress()));
 
 			// アニメーションの9割りが終了していた場合、スティックの入力があった場合待機状態へ移行
 			if (!player_->skiningModels_[0]->animationManager_.GetIsPlayingAnimation() && !isDead_) {
